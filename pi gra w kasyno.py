f96bc0e17cb8c3b3ee498415967b7cfd957905b0
DJBio <73431002+DJBio@users.noreply.github.com>
2020-12-09 10:37:16 +0100

    Update pi gra w kasyno.py

--- pi gra w kasyno.py
+++ pi gra w kasyno.py
@@ -1,12 +1,12 @@
 
 from random import random
-from math import (sqrt, pi)
+from math import pi
 
 def calc_pi(ile_prob):
     """Funkcja wyliczająca liczbę pi metodą Monte Carlo"""
     wewn = 0
     for x in range(ile_prob):
-        if sqrt(random()**2 + random()**2) <= 1:
+        if random()**2 + random()**2 <= 1:
             wewn +=1    
     oszac = 4*(wewn/ile_prob)
     return "Oszacowanie pi:{:.5f}. Błąd: {:.3f}%".format(oszac, abs((oszac/pi)-1)*100)
