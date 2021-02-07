924773a16a5bd89f4ab8a97a8c04f1b7088f2955
DJBio <73431002+DJBio@users.noreply.github.com>
2021-02-07 09:45:35 +0100

    Update DRUK DWUSTRONNY.py

--- DRUK DWUSTRONNY.py
+++ DRUK DWUSTRONNY.py
@@ -1,4 +1,7 @@
-from math import ceil
+import time
+from math import (ceil, floor)
+import pyperclip
+
 while True:
     start_raw = input ("Podaj początkową stonę; [Enter] => 1\n")
     if start_raw == '':
@@ -7,7 +10,8 @@ while True:
         start = int(start_raw)
 
     stop = int(input ("Podaj końcową stronę\n"))
-    ile_raw = input ("Podaj ilość stron na stronicę arkuszu; [Enter] => 4\n")
+    ile_raw = input ("Podaj ilość stron na stronicy drukowanego arkusza;"
+        "[Enter] => 4\n")
     if ile_raw == '':
         ile = 4
     else:
@@ -29,5 +33,16 @@ while True:
             licznik = 1
     print('I: {}-Liczba arkuszy:{}'.format(I, ceil (len(I)/ile)))
     print('II:{}-Liczba arkuszy:{}\n'.format(II, ceil (len(II)/ile)))
+    
+
     if ceil (len(I)/ile) != ceil (len(II)/ile):
-        print('UWAGA na ostatnim arkuszu będzie wydrukowano:_{} STRON_ z {} możliwych \n\n'.format(len(I)-len(II),2*ile))
+        print('\n{:_^80s}'.format('UWAGA'))
+        print('\n{: ^80s}'.
+            format('OSTATNI ARKUSZ BĘDZIE TYLKO CZĘŚCIOWO ZADRUKOWANY'))
+        print('\nZadrukowanie:|{:-<50s}| ({} / {})'.
+            format('█'*floor((len(I)-len(II))/(2*ile)*50), len(I)-len(II),2*ile))
+        print('_'*80, '\n\n')
+    pyperclip.copy(','.join(map(str, II)))
+    time.sleep(0.5) # dlaczegoś w Schowku nie będzie zapisany 'II',
+                    #    jeżeli skopiować od razu 'I'
+    pyperclip.copy(','.join(map(str, I)))
